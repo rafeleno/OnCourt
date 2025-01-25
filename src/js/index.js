88d4b3a2ca928ccfeeed8f383acb9f0477e82068
Victor Baskakov <viktorbaskakov356@gmail.com>
2025-01-26 01:56:47 +0300

    Another commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,14 @@ import * as ymaps3 from 'ymaps3';
 async function initMap() {
   await ymaps3.ready;
 
-  const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
 
-  const map = new YMap(
-      document.getElementById('map'),
-      {
-          location: {
-              center: [30.608271, 59.738259],
-              zoom: 15
-          }
-      }
-  );
+  const map = new YMap(document.getElementById('map'), {
+    location: {
+      center: [30.608271, 59.738259],
+      zoom: 14
+    }
+  });
 
   map.addChild(new YMapDefaultSchemeLayer());
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,65 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, "src"),
-    mode: "development",
-    entry: {
-        main: "./js/index.js",
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './js/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
     },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-        }),
+    compress: true,
+    port: 9000,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'https://oncourt.com',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
     ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
-    },
-    externals: {
-        ymaps3: 'ymaps3'
-    },
-    devtool: 'cheap-source-map'
+    host: 'local.oncourt.com', // Добавьте эту строку
+    https: true // Если требуется HTTPS
+  },
+  externals: {
+    ymaps3: 'ymaps3'
+  },
+  devtool: 'cheap-source-map'
 };
